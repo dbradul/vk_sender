0792281910c36674b5076c7dd956bdc6bc6bc3fe
Bradul Dmytro <dbradul@gmail.com>
2022-04-07 12:12:53 +0300

    Added video upload support

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 import time
 
 import requests
+import vk_api
 from dotenv import load_dotenv
 from openpyxl import load_workbook
 from openpyxl.worksheet.worksheet import Worksheet
@@ -14,18 +15,42 @@ from utils import logger, login_retrier, repack_exc
 
 load_dotenv()
 
+VIDEO_ENDINGS = ['.mp4']
 
 @login_retrier
 @repack_exc
-def send_message(client, user_id, message, mediaurl):
+def send_message_video(client: VkClientProxy, user_id, message, mediaurl):
+    vk_uploader = vk_api.upload.VkUpload(client._obj)
+
+    res_upload_video = vk_uploader.video(
+        video_file=mediaurl,
+        is_private=True
+    )
+
+    media_file_id = f'video{res_upload_video["owner_id"]}_{res_upload_video["video_id"]}'
+
+    res_send = client.messages.send(
+        user_id=user_id,
+        random_id=random.randint(100, 100000000),
+        message=message,
+        attachment=media_file_id
+    )
+
+    return res_send
+
+
+@login_retrier
+@repack_exc
+def send_message_photo(client, user_id, message, mediaurl):
 
     album_id = 0
-    if client.owner_id:
-        res_get_albums = client.photos.getAlbums(
-            owner_id=client.owner_id
-        )
-        if res_get_albums.items:
-            album_id = res_get_albums['items'][0]['id']
+    res_get_albums = client.photos.getAlbums(
+        # owner_id=client.owner_id
+    )
+
+    if res_get_albums.items:
+        album_id = res_get_albums['items'][0]['id']
+        client.owner_id = res_get_albums['items'][0]['owner_id']
 
     if not album_id:
         res_create_album = client.photos.createAlbum(
@@ -94,7 +119,10 @@ def process_file(client, filename):
             mediaurl = ws[f'H{str(r)}'].value
             user_name = vkurl.split('/')[-1]
             user_id = get_user_id_by_name(client, user_name)
-            resp = send_message(client, user_id=user_id, message=message, mediaurl=mediaurl)
+            if any([True for elem in VIDEO_ENDINGS if mediaurl.lower().endswith(elem)]):
+                resp = send_message_video(client, user_id=user_id, message=message, mediaurl=mediaurl)
+            else:
+                resp = send_message_photo(client, user_id=user_id, message=message, mediaurl=mediaurl)
             ws[f'J{str(r)}'].value = resp
             logger.info(f'Processed {vkurl} with result {resp}')
             time_left_to_sleep_sec = max(10 - (time.time() - start_time), 0)
